d35ab784e2500ccc5fbeb5c36cb04272732b2b71
Justin ( Cogware) <94659698+NINJAGECKO5000@users.noreply.github.com>
2024-05-29 19:25:05 -0700

    Add files via upload

---
+++ target/aarch64-unknown-none-softfloat/release/deps/bare_metal_spaceinvaders-3c65f96d938d85ca.d
@@ -0,0 +1,13 @@
+/home/ninja/Desktop/strippedpi/target/aarch64-unknown-none-softfloat/release/deps/bare_metal_spaceinvaders-3c65f96d938d85ca: kernel/src/main.rs kernel/src/boot/mod.rs kernel/src/framebuffer.rs kernel/src/logger.rs kernel/src/mailbox.rs kernel/src/print.rs kernel/src/time.rs kernel/src/uart_pl011.rs kernel/src/boot/boot.s
+
+/home/ninja/Desktop/strippedpi/target/aarch64-unknown-none-softfloat/release/deps/bare_metal_spaceinvaders-3c65f96d938d85ca.d: kernel/src/main.rs kernel/src/boot/mod.rs kernel/src/framebuffer.rs kernel/src/logger.rs kernel/src/mailbox.rs kernel/src/print.rs kernel/src/time.rs kernel/src/uart_pl011.rs kernel/src/boot/boot.s
+
+kernel/src/main.rs:
+kernel/src/boot/mod.rs:
+kernel/src/framebuffer.rs:
+kernel/src/logger.rs:
+kernel/src/mailbox.rs:
+kernel/src/print.rs:
+kernel/src/time.rs:
+kernel/src/uart_pl011.rs:
+kernel/src/boot/boot.s:
---
+++ target/aarch64-unknown-none-softfloat/release/deps/log-b34e3044c3e5894f.d
@@ -0,0 +1,9 @@
+/home/ninja/Desktop/strippedpi/target/aarch64-unknown-none-softfloat/release/deps/liblog-b34e3044c3e5894f.rmeta: /home/ninja/.cargo/registry/src/index.crates.io-6f17d22bba15001f/log-0.4.19/src/lib.rs /home/ninja/.cargo/registry/src/index.crates.io-6f17d22bba15001f/log-0.4.19/src/macros.rs /home/ninja/.cargo/registry/src/index.crates.io-6f17d22bba15001f/log-0.4.19/src/serde.rs
+
+/home/ninja/Desktop/strippedpi/target/aarch64-unknown-none-softfloat/release/deps/liblog-b34e3044c3e5894f.rlib: /home/ninja/.cargo/registry/src/index.crates.io-6f17d22bba15001f/log-0.4.19/src/lib.rs /home/ninja/.cargo/registry/src/index.crates.io-6f17d22bba15001f/log-0.4.19/src/macros.rs /home/ninja/.cargo/registry/src/index.crates.io-6f17d22bba15001f/log-0.4.19/src/serde.rs
+
+/home/ninja/Desktop/strippedpi/target/aarch64-unknown-none-softfloat/release/deps/log-b34e3044c3e5894f.d: /home/ninja/.cargo/registry/src/index.crates.io-6f17d22bba15001f/log-0.4.19/src/lib.rs /home/ninja/.cargo/registry/src/index.crates.io-6f17d22bba15001f/log-0.4.19/src/macros.rs /home/ninja/.cargo/registry/src/index.crates.io-6f17d22bba15001f/log-0.4.19/src/serde.rs
+
+/home/ninja/.cargo/registry/src/index.crates.io-6f17d22bba15001f/log-0.4.19/src/lib.rs:
+/home/ninja/.cargo/registry/src/index.crates.io-6f17d22bba15001f/log-0.4.19/src/macros.rs:
+/home/ninja/.cargo/registry/src/index.crates.io-6f17d22bba15001f/log-0.4.19/src/serde.rs:
