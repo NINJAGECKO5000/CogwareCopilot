7e7f17455cc67ac220181314da751c41cde33021
Justin ( Cogware) <94659698+NINJAGECKO5000@users.noreply.github.com>
2024-06-02 17:40:34 -0700

    Update color.rs

--- space_invaders/src/framebuffer/color.rs
+++ space_invaders/src/framebuffer/color.rs
@@ -21,8 +21,7 @@ impl Color {
     }
 }
 
-// pub const BLACK_COLOR: Color = Color::new(0, 0, 0);
-
+pub const BLACK_COLOR: Color = Color::new(0, 0, 0);
 pub const WHITE_COLOR: Color = Color::new(255, 255, 255);
 pub const BLUE_COLOR: Color = Color::new(0, 0, 255);
 pub const SHOT_COLOR: Color = Color::new(252, 186, 3);
