4f4ca12615a41c879d2689acdaf5d303ecf9c05a
Leah Lynn <lynndoxxy@gmail.com>
2024-09-10 23:10:55 -0700

    add interrupt enable/disable/check for mailbox

--- bcm2837-hal/src/interrupt.rs
+++ bcm2837-hal/src/interrupt.rs
@@ -2,6 +2,31 @@ use core::sync::atomic::{compiler_fence, Ordering};
 
 use interrupts::Guard;
 
+#[derive(Debug)]
+pub struct LicmaHandler;
+
+impl core::ops::Deref for LicmaHandler {
+    type Target = crate::pac::lic::RegisterBlock;
+
+    fn deref(&self) -> &Self::Target {
+        unsafe { &*(crate::pac::LIC::ptr() as *const Self::Target) }
+    }
+}
+
+impl LicmaHandler {
+    pub fn enable_mailbox() {
+        Self.enable_basic().modify(|_, w| w.mailbox().bit(true));
+    }
+
+    pub fn disable_mailbox(&self) {
+        Self.disable_basic().modify(|_, w| w.mailbox().bit(true));
+    }
+
+    pub fn mailbox_pending(&self) -> bool {
+        Self.basic_pending().read().mailbox().bit_is_set()
+    }
+}
+
 #[inline]
 pub fn disable() -> Guard {
     let guard = interrupts::disable();
